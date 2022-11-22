20955a78546aa936e8f04b06fb05496e3d059f15
Aniket Shukla <anishukla666@gmail.com>
2022-11-22 16:32:29 +0530

    Storing large image in DB and using jFileChooser to upload image

--- JDBC-Learning-Store-large-Image/src/com/jdbc/learning/storing/large/image/Connector.java
+++ JDBC-Learning-Store-large-Image/src/com/jdbc/learning/storing/large/image/Connector.java
@@ -12,7 +12,7 @@ public class Connector {
 			try {
 				
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/StoreImage","root","Dravid@1996");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/StoreImage","root","ENTER_YOUR_DB_PASSWORD");
 				
 			} catch (Exception e) {
 				System.out.println(e);
