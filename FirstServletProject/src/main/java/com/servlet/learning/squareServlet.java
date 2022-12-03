1685de0ebaafdd5b2cb9304d84e6b0bb9a1eb95b
Aniket Shukla <anishukla666@gmail.com>
2022-12-03 14:54:46 +0530

    JSP learning-basics

--- FirstServletProject/src/main/java/com/servlet/learning/squareServlet.java
+++ FirstServletProject/src/main/java/com/servlet/learning/squareServlet.java
@@ -3,12 +3,13 @@ package com.servlet.learning;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/square")
 public class squareServlet extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
