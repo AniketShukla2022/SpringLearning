8d4d069b3183758266afa7d97ffddac3c7bfca5f
Aniket Shukla <anishukla666@gmail.com>
2022-12-03 13:59:31 +0530

    Servlet learning- basics

---
+++ FirstServletProject/src/main/java/com/servlet/learning/squareServlet.java
@@ -0,0 +1,28 @@
+package com.servlet.learning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class squareServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
+		
+		for (Cookie c : cookies) {
+			if (c.getName().equals("k"))
+				k = Integer.parseInt(c.getValue());
+		}
+		
+		k = k*k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Square of the sum is: "+k);
+	}
+}
